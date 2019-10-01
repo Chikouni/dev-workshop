09e2bc48ad8bd0f90c9d0033ef93ad685f7754da
Julian <husson.j@yahoo.com>
2019-10-01 11:13:17 +0200

    Footer - header defined

--- workshop/src/app/global/layouts/header/header.component.ts
+++ workshop/src/app/global/layouts/header/header.component.ts
@@ -7,15 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  toggleOpen = false;
-
   constructor() { }
 
   ngOnInit() {
   }
   
-  toggleNav(){
-    this.toggleOpen = !this.toggleOpen;
-    console.log(this.toggleOpen)
-  }
 }
