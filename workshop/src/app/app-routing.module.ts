153505ace270c8292788cee7a792fe8f52d71098
Chikouni <louisdoubi@gmail.com>
2019-10-01 17:06:48 +0200

    LUI/ Form control

--- workshop/src/app/app-routing.module.ts
+++ workshop/src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DefisComponent } from './components/defis/defis.component';
-
+import { AccessComponent } from './components/access/access.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent },
-  { path: 'defis', component: DefisComponent }
+  { path: 'defis', component: DefisComponent },
+  { path: 'access', component: AccessComponent }
 ];
 
 @NgModule({
