132d63c1da97bd6348a281bf40bf070024270d31
Chikouni <louisdoubi@gmail.com>
2019-10-03 01:11:53 +0200

    LOUIS/ contact

--- workshop/src/app/components/contact/contact.component.ts
+++ workshop/src/app/components/contact/contact.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ContactComponent implements OnInit {
 
