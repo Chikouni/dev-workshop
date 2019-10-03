5fd1a69c542bfddd9ad098fad1df6b16bcdd85ba
Chikouni <louisdoubi@gmail.com>
2019-10-03 18:55:49 +0200

    LOUIS FINAL

--- workshop/src/app/components/defis/defis.component.ts
+++ workshop/src/app/components/defis/defis.component.ts
@@ -1,11 +1,13 @@
 import { DefisService } from './../../global/services/defis.service';
 import { Defi } from './../../global/models/defi.model';
 import { Component, OnInit } from '@angular/core';
+import {NgbProgressbarConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-defis',
   templateUrl: './defis.component.html',
-  styleUrls: ['./defis.component.scss']
+  styleUrls: ['./defis.component.scss'],
+  providers: [NgbProgressbarConfig]
 })
 export class DefisComponent implements OnInit {
 defis: Defi[] = []
@@ -14,8 +16,13 @@ userData = localStorage.getItem('caeliUserData');
 currentLvl: number = JSON.parse(this.userData).level;
 width = 39 ;
 
-  constructor( private defisService: DefisService) {}
-
+  constructor( private defisService: DefisService, config: NgbProgressbarConfig) {
+    config.max = 371;
+    config.striped = true;
+    config.animated = true;
+    config.type = 'success';
+    config.height = '20px';
+  }
   ngOnInit() {
     this.defisService.getDefis().subscribe(defis => {
       this.defis = defis;
